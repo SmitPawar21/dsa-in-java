d34b9f84e578f636135158900b9aceb20f56bad2
Smit Pawar <dynaster21@gmail.com>
2024-08-29 01:42:25 +0530

    Time: 1 ms (44.61%), Space: 40.3 MB (77.52%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=1; i<=n; i++)
+        {
+            if(n%i == 0)
+            {
+                list.add(i);
+            }
+        }
+
+        if(k > list.size())
+        {
+            return -1;
+        }
+        int answer = list.get(k-1);
+        return answer;
+    }
+}
